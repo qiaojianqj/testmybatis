58ae938afb456e2c266017659318cdfe1c21ab9e
GaGa <gaga@GaGadeBook.lan>
2019-04-28 10:43:09 +0800

    add transactional test

--- src/main/java/com/leo/RoleMapperUse.java
+++ src/main/java/com/leo/RoleMapperUse.java
@@ -5,6 +5,7 @@ import com.leo.domain.Role;
 import com.leo.domain.RoleExample;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.PostConstruct;
 
@@ -29,5 +30,22 @@ public class RoleMapperUse {
         roleExample.createCriteria ().andNoEqualTo ( "manager" ).andSeqidEqualTo ( 1L );
         int count = roleMapper.updateByExampleSelective ( role,  roleExample);
         System.out.println ( "count: " + count );
+    }
+
+    @Transactional
+    public void  throwInTransactional() {
+        Role role = new Role ();
+        role.setSeqid ( 3L );
+        role.setName ( "谯剑" );
+        role.setNo ( "xiaoluoluo" );
+        role.setType ( 2 );
+        role.setRemark ( "小螺螺" );
+        int count = roleMapper.insert ( role );
+        System.out.println ( "count: " + count );
+        //加了Transactional注解，碰到异常会回滚数据库事务
+        //没加注解，碰到异常也插入成功
+        if (true) {
+            throw  new RuntimeException ( "故意抛出异常" );
+        }
     }
 }
